704caac9e6a7425c89c6054064b5603756e559c3
jvalters <40427504+jvalters@users.noreply.github.com>
2019-06-20 09:23:34 +0300

    Update and rename Check MySQL status if down start to mysql-checker.sh

--- mysql-checker.sh
+++ mysql-checker.sh
@@ -2,7 +2,7 @@
 ###                                                                  ###
 ###          Writen by Janis Valters                                 ###
 ###  Website: https://www.valters.eu                                 ###
-###  GITHUB:  https://github.com/jvalters/automationScripts_mysql    ###
+###  GITHUB:  https://github.com/jvalters/                           ###
 ###                                                                  ###
 ###                                                                  ###
 ###         Tested on Centos 7                                       ###
@@ -10,21 +10,28 @@
 
 !/bin/bash
 #Send e-mail to who
+
 email='some_email@e-mail.com'
+
 #E-mail Subject
 subject='Subject: Project name - mysql process down and up'
+
 #MySQL start service
 MYSQL_START='sudo service mysql start'
+
 #Search for MySQL PID
 MYSQL='mysqld'
 PGREP='/usr/bin/pgrep'
+
 #Check if MySQL using PID is working
 $PGREP $MYSQL
 
 #If MySQL responses 0 send a signal to start MySQL server and send e-mail to notify an admin.
 if [ $? -ne 0 ]; then
+
 #Start MySQL
 $MYSQL_START
+
 #Send notification
 echo "$subject" | /usr/sbin/sendmail $email
 fi
